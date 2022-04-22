6b286c88f13de1db6721957330aaa237130f60d3
leonard-porteria <porterialeonard@gmail.com>
2022-04-22 14:02:11 +0800

    add variables in cocktail.js

--- algorithms/cocktail.js
+++ algorithms/cocktail.js
@@ -2,4 +2,12 @@ import { speed } from "../app.js";
 
 export async function cocktailShaker() {
   console.log("Cocktail Shaker Function");
+  // VARIABLES
+  const divEl = document.querySelectorAll(".visualizer__contianer__element");
+  const divHeight = [];
+
+  // STORE HEIGHT
+  for (let i = 0; i < divEl.length; i++) {
+    divHeight[i] = divEl[i].offsetHeight;
+  }
 }
