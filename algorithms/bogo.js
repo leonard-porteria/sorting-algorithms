7ec2538f367e8109f6dc159ee19e501944cfd4b1
leonard-porteria <porterialeonard@gmail.com>
2022-04-30 19:52:06 +0800

    reduced sorting algorithms for next update

--- algorithms/bogo.js
+++
@@ -1,5 +0,0 @@
-import { speed } from "../app.js";
-
-export async function bogoSort() {
-  console.log("Bogo Sort Function");
-}
--- algorithms/radix.js
+++
@@ -1,5 +0,0 @@
-import { speed } from "../app.js";
-
-export async function radixSort() {
-  console.log("Radix Sort Function");
-}
